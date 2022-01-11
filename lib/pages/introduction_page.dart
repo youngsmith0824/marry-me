3965079d40ce476a2720e2845e4ed314ea2bd372
youngsmith0824 <yyjjang9@naver.com>
2022-01-12 03:14:42 +0900

    scroll 추가

--- lib/pages/introduction_page.dart
+++ lib/pages/introduction_page.dart
@@ -10,16 +10,41 @@ class Introduction extends StatefulWidget {
 }
 
 class _IntroductionState extends State<Introduction> {
+  final List<IntroductionCard> colorCodes = const [
+    IntroductionCard(primaryProfile: '장영연 (1)', secondaryProfile: '프로그래머, 강남구, 182cm, 통통'),
+    IntroductionCard(primaryProfile: '김현수 (2)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (3)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (4)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (5)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (6)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (7)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (8)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (9)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (11)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (12)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (13)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (13)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (14)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (15)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (16)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+    IntroductionCard(primaryProfile: '김현수 (17)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const DefaultAppBar(),
-      body: Column(
-        children: const [
-          IntroductionCard(primaryProfile: '장영연 (31)', secondaryProfile: '프로그래머, 강남구, 182cm, 통통'),
-          IntroductionCard(primaryProfile: '김현수 (31)', secondaryProfile: '프로그래머, 송파, 180cm, 마름'),
-        ],
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: colorCodes.length,
+          itemBuilder: (BuildContext context, int index) {
+            return colorCodes[index];
+          }
       ),
     );
   }
 }
+
+/*
+
+ */
